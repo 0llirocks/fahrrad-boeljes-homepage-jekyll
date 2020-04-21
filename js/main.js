8ef60e441c90baf7a013331f063bf856993d04c1
Oliver Hamboerger <oliver.hamboerger@gmail.com>
2020-04-21 10:55:05 +0200

    Second draft

--- js/main.js
+++ js/main.js
@@ -8,18 +8,5 @@ $(document).ready(function(){
 		fade: true,
 		speed: 1000
 	});
-    
-    $(".collapsible-trigger > a").click(function() {
-    	$(".collapsible-trigger").removeClass("active");
-    	$(this).parent().addClass("active");
-    	$(".collapsible").hide();
-    	$(this).next(".collapsible").show();
-    	
-    });
-    
-    $(".collapsible > li > a").click(function() {
-    	$(".collapsible > li").removeClass("active");
-    	$(this).parent().addClass("active");
-    });
-   
+	 
 });
