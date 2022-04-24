60899b65c890996159c1f0c48b81d73d4e97d118
0llirocks <oliver.hamboerger@gmail.com>
2022-04-24 16:57:43 +0200

    Add popup

--- js/main.js
+++ js/main.js
@@ -8,5 +8,23 @@ $(document).ready(function(){
 		fade: true,
 		speed: 1000
 	});
+
+	var popupEmpty = $("#popup-content-title").html() === "";
+
+	if(popupEmpty)
+	{
+		sessionStorage.clear();
+	}
+	else if (sessionStorage.popup === undefined) {
+		$('#popup').css('display', 'flex');
+		sessionStorage.popup = 0;
+	}
+
+	$("#popup-content-close, #popup").click(function(e) {
+		if(e.target === e.currentTarget)
+		{
+			$("#popup").fadeOut();
+		}
+	});
 	 
 });
