567290494adb60cce541325d3b0bde3ca3c8e4f5
0llirocks <oliver.hamboerger@gmail.com>
2023-02-05 20:06:51 +0100

    Update to Vermietung Boeljes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,5 @@
 $(document).ready(function(){
 
-	$('.slick').slick({
-		autoplay: true,
-		autoplaySpeed: 7000,
-		pauseOnHover: false,
-		arrows: false,
-		fade: true,
-		speed: 1000
-	});
-
-	var popupEmpty = $("#popup-content-title").html() === "";
-
-	if(popupEmpty)
-	{
-		sessionStorage.clear();
-	}
-	else if (sessionStorage.popup === undefined) {
-		$('#popup').css('display', 'flex');
-		sessionStorage.popup = 0;
-	}
-
 	$("#popup-content-close, #popup").click(function(e) {
 		if(e.target === e.currentTarget)
 		{
